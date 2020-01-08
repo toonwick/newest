2464da06cffb11374dd0be756c466574af11a548
Jack <jack@divo>
2020-01-08 20:13:50 +0700

    api connect

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ create backend [https://expressjs.com/en/starter/generator.html]
 create frontend [https://create-react-app.dev/]
 create npm init [https://docs.npmjs.com/cli/init]
 create concurrently [https://www.npmjs.com/package/concurrently]
-change backend port 3000 -> 5000
+
+change backend port 3000 to 5000
+
+touch .gitignore && echo "node_modules/" >> .gitignore && git rm -r --cached node_modules ; git status
 
 git push [https://help.github.com/en/github/importing-your-projects-to-github/adding-an-existing-project-to-github-using-the-command-line]
+
+frontend connect backend [https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/]
+
+
--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -15,12 +17,14 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
