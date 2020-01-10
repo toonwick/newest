e41c0dbb79aeaee7b5f0c59bc1f77b45da7b7b43
Jack <jack@divo>
2020-01-10 23:04:05 +0700

    Consume an API

---
+++ frontend/src/components/image-results.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ImageResults extends Component {
+  state = {
+
+  };
+
+  handleOpen = img => {
+
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+ImageResults.propTypes = {
+  images: PropTypes.array.isRequired
+};
+
+export default ImageResults;
---
+++ frontend/src/components/search.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import ImageResults from '../image-results';
+
+class Search extends Component {
+  state = {
+
+  };
+
+  onTextChange = e => {
+
+  };
+
+  onAmountChange = (e, index, value) => this.setState({ amount: value });
+
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default Search;
